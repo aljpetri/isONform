aede3e10f46fd4f81501e7e6329a16a8d0e88d77
aljpetri <aljpetri@gmail.com>
2023-04-13 11:03:49 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,106 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# using example setup file from https://github.com/pypa/sampleproject/blob/master/setup.py
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+
+    name='isONform',  # Required
+    version='0.1.0',  # Required
+    description='De novo construction of isoforms from long-read data ',  # Required
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',
+    url='https://github.com/aljpetri/isONform',  # Optional
+    author='Alexander Petri',  # Optional
+    author_email='alexander.petri@math.su.se',  # Optional
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        #'Intended Audience :: Developers',
+        #'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        #'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        #'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+
+    keywords='Oxford Nanopore isoform prediction',  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    # If your package is for Python 2.7, and all versions of Python 3 starting with 3.4, write
+    python_requires='>=3.7',
+#!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['recordclass>=0.17.2',
+                     'networkx>=2.7.1','parasail>=1.3.3','edlib>=1.1.2'],#'pyinstrument>4.1.1'],  # Optional
+    # dependency_links=[], # Optional
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    # entry_points={  # Optional
+    #     'console_scripts': [
+    #         'IsoCon=IsoCon.__main__()',
+    #     ],
+    # },
+    scripts=['isONform_parallel.py'],
+)
