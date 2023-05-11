65365b13f37ea05ffc1ffad70bc7d54a221d1908
aljpetri <aljpetri@gmail.com>
2023-05-11 11:27:38 +0300

    Redownload every time

--- tests/download.sh
+++ tests/download.sh
@@ -8,14 +8,14 @@ cd tests/drosophila
 mkdir -p 0
 cd 0
 
-if ! [[ -e corrected_reads.fastq ]]; then
+#if ! [[ -e corrected_reads.fastq ]]; then
   curl -o corrected_reads.fastq https://github.com/aljpetri/isONform_analysis/blob/main/test_data/SIRV_50_isos_cl0.fastq
   #mv ref.fastq.tmp corrected_reads.fastq
   #echo "$PWD"
   echo "$(head -20 corrected_reads.fastq)"
   #echo cat my_file.txt
 
-fi
+#fi
 if ! [[ -e ref.fasta ]]; then
   curl https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz | gunzip > ref.fasta.tmp
   mv ref.fasta.tmp ref.fasta
