a2cc2c2a5a81fdc10329dc6e7fa6f53e68e9b8ca
aljpetri <aljpetri@gmail.com>
2023-05-11 16:49:37 +0300

    Removed test case for download.sh

--- tests/download.sh
+++ tests/download.sh
@@ -16,11 +16,11 @@ cd 0
   #echo cat my_file.txt
 
 #fi
-if ! [[ -e ref.fasta ]]; then
-  curl https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz | gunzip > ref.fasta.tmp
-  mv ref.fasta.tmp ref.fasta
-  echo "$(head -20 ref.fasta)"
-fi
+#if ! [[ -e ref.fasta ]]; then
+#  curl https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz | gunzip > ref.fasta.tmp
+#  mv ref.fasta.tmp ref.fasta
+#  echo "$(head -20 ref.fasta)"
+#fi
 #for r in 1 2; do
 #    f=reads.${r}.fastq.gz
 #    if ! [[ -e ${f} ]]; then
