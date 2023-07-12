bef79e53d99bce7cd89eb2bedace259d25a039bb
aljpetri <aljpetri@gmail.com>
2023-07-12 22:02:46 +0300

    Fixed bug for running main that wrongly took args.delta

--- main.py
+++ main.py
@@ -509,7 +509,7 @@ def main(args):
                 for other_id,other_val in value.items():
                    print(id,": ",other_id,":",other_val[0],"::",other_val[1])
 
-        mode=args.slow
+        mode = args.slow
         #the bubble popping step: We simplify the graph by linearizing all poppable bubbles
         SimplifyGraph.simplifyGraph(DG, new_all_reads, work_dir, k_size, delta_len, mode)
         #TODO: add delta as user parameter possibly?
@@ -525,7 +525,7 @@ def main(args):
     if not args.parallel:
             print("Merging the batches with linear strategy")
             #merges the predictions from different batches
-            batch_merging_parallel.join_back_via_batch_merging(args.outfolder, args.delta, args.delta_len, args.merge_sub_isoforms_3,
+            batch_merging_parallel.join_back_via_batch_merging(args.outfolder, delta, args.delta_len, args.merge_sub_isoforms_3,
                                                                args.merge_sub_isoforms_5, args.delta_iso_len_3, args.delta_iso_len_5,
                                                                args.max_seqs_to_spoa, args.iso_abundance)
     print("removing temporary workdir")
