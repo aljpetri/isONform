aljpetri/isONform

commit 265865f93cd829d0549acb72b5c26574a1918f4f

Installing recordclass is not a smooth experience on a macbook. Ths PR removes the recordclass dependency by creating a struct-like class with the same name

modules/batch_merging_parallel.py
@@ -1,11 +1,15 @@
 import itertools
 import pickle
-from recordclass import *
 import os
 
 from modules import IsoformGeneration
 from modules import Parallelization_side_functions
 
+class Read:
+    def __init__(self, sequence, reads, merged):
+        self.sequence = sequence
+        self.reads = reads
+        self.merged = merged
 
 def generate_consensus_path(work_dir, mappings1, mappings2, all_sequences, spoa_count):
     """ This method is used to generate the consensus file needed for the consensus generation
@@ -193,7 +197,6 @@ def actual_merging_process(all_infos_dict, delta, delta_len,
 def join_back_via_batch_merging(outdir, delta, delta_len, delta_iso_len_3,
                                 delta_iso_len_5, max_seqs_to_spoa, iso_abundance):
     print("Batch Merging")
-    Read = recordclass('Read', "sequence reads merged")
     unique_cl_ids = set()
     subfolders = [f.path for f in os.scandir(outdir) if f.is_dir()]
     # iterate over all folders in the out directory

modules/SimplifyGraph.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-import recordclass
 import itertools
 import networkx as nx
 import os
@@ -7,6 +6,11 @@ import os
 from modules import consensus
 from modules import IsoformGeneration
 
+class Readtup:
+    def __init__(self, path, supp, non_supp):
+        self.path = path
+        self.supp = supp
+        self.non_supp = non_supp
 
 Read_infos = namedtuple('Read_Infos',
                         'start_mini_end end_mini_start original_support')
@@ -796,7 +800,6 @@ def filter_path_if_marked(marked, path):
 
 def find_combi_paths(combination, all_paths):
     # TODO change this function to find the paths for a combination and return the correct value
-    Readtup = recordclass('Readtup', 'path supp non_supp')
     # all_r_ids=set(all_paths.keys())
     startnode = combination[0]
     endnode = combination[1]

commit 4171b361dc8977fefdbf371159f531bdab261db1

Fixed error introduced by commit 265865f

modules/batch_merging_parallel.py
@@ -143,9 +143,9 @@ def actual_merging_process(all_infos_dict, delta, delta_len,
                            delta_iso_len_3, delta_iso_len_5, max_seqs_to_spoa, all_batch_sequences, work_dir):
     all_infos_list = sorted(all_infos_dict.items(), key=lambda x: x[0], reverse=True)
     for b_i, (batchid, id_dict) in enumerate(all_infos_list[:len(all_infos_list) - 1]):
-        batch_id_list = sorted(id_dict.items(), key=lambda x: len(x[1]))
+        batch_id_list = sorted(id_dict.items(), key=lambda x: len(x[1].sequence))
         for b_j, (batchid2, id_dict2) in enumerate(all_infos_list[b_i + 1:]):
-            batch_id_list2 = sorted(id_dict2.items(), key=lambda x: len(x[1]))
+            batch_id_list2 = sorted(id_dict2.items(), key=lambda x: len(x[1].sequence))
             if not batchid2 <= batchid:
                 for t_id1, (id, infos) in enumerate(batch_id_list):
                     if not infos.merged:
@@ -273,7 +273,7 @@ def main():
     # outfolder = "/home/alexanderpetri/isONform_analysis/Para_out_500batchadd"
     # outfolder="/home/alexanderpetri/isONform_analysis/Batch_parallel_testing"
     # outfolder = "/home/alexanderpetri/isONform_analysis/Para_out_500batch"
-    outfolder = "/home/alexanderpetri/isONform_analysis/Para_out_500_September"
+    outfolder = "/home/alexanderpetri/Desktop/IsONform_test_results/SIRV100k/552997d_t2"
     delta = 0.15
     delta_len = 5
     merge_sub_isoforms_3 = True
@@ -282,8 +282,9 @@ def main():
     delta_iso_len_5 = 50
     max_seqs_to_spoa = 200
     iso_abundance = 4
-    join_back_via_batch_merging(outfolder, delta, delta_len, merge_sub_isoforms_3, merge_sub_isoforms_5,
-                                delta_iso_len_3, delta_iso_len_5, max_seqs_to_spoa, iso_abundance)
+    join_back_via_batch_merging(outfolder, delta, delta_len, delta_iso_len_3, delta_iso_len_5,
+                                max_seqs_to_spoa, iso_abundance)
+
     Parallelization_side_functions.generate_full_output(outfolder)
     # merge_batches(max_batchid, tmp_work_dir, outfolder, all_reads,merge_sub_isoforms_3,merge_sub_isoforms_5, delta, delta_len,max_seqs_to_spoa, delta_iso_len_3, delta_iso_len_5,iso_abundance)
     print("removing temporary workdir")