6d82cc92116970afb8df0985011615ecf7662a95
aljpetri <aljpetri@gmail.com>
2024-11-18 16:00:02 +0200

    Version 0.3.8: Removed --clustered from isONform as now unneccessary, fixed running isONform without split_wrt_batches

--- isON_pipeline.sh
+++ isON_pipeline.sh
@@ -5,7 +5,7 @@ set -e
 ####ONT data
 # ont_with_pychopper: the full pipeline is run in addition to pychopper (pychopper, isONclust,isONcorrect,isONform)
 # ont_no_pychopper: only the isONpipeline is run without pychopper (isONclust,isONcorrect, isONform)
-
+# ont_no_pychop_isonclust3
 ####PACBIO data
 # pacbio: for PacBio data runs isONclust and isONform
 
@@ -78,9 +78,18 @@ echo "Running `basename $0` raw reads: '$raw_reads' outfolder: '$outfolder' num_
 
 mkdir -p $outfolder
 ##Testing whether the programs are installed properly before attempting to run them
-pychopper --h
+# shellcheck disable=SC1072
+if [ $mode != "pacbio" ] && [ $mode != "ont_no_pychop" ]; #we do not want to test for pychopper if we run modes pacbio or ont_no_pychop
+then
+  pychopper --h
+fi
 isONclust
-run_isoncorrect
+#only test for isONcorrect if we want to use it (so not neccessary for Pacbio data)
+if [ $mode != "pacbio" ]
+then
+  run_isoncorrect
+fi
+
 if [ -n "$isONform_folder"  ] #the user has given a path to isONform (cloned from github)
   then
   $isONform_folder/isONform_parallel --h
@@ -126,16 +135,19 @@ if [ $mode != "only_isonform" ] # this if statement prevents isONclust and isONc
              --outfolder $outfolder/clustering
        /usr/bin/time -v isONclust write_fastq --N $iso_abundance --clusters $outfolder/clustering/final_clusters.tsv \
                       --fastq $raw_reads --outfolder  $outfolder/clustering/fastq_files
-  elif [ $mode == "pacbio" ]#This is the pacbio mode
+  elif [ $mode == "pacbio" ] #This is the pacbio mode
     then
        /usr/bin/time -v  isONclust  --t $num_cores  --isoseq  --fastq $raw_reads \
              --outfolder $outfolder/clustering
        /usr/bin/time -v isONclust write_fastq --N $iso_abundance --clusters $outfolder/clustering/final_clusters.tsv \
                       --fastq $raw_reads --outfolder  $outfolder/clustering/fastq_files
 
+  #elif [ $mode == "ont_no_pychop_isonclust3" ]
+  #  then
+  #    /home/alexanderpetri/Rust/isONclust_rs/target/release/isONclust3  --mode ont --fastq $raw_reads --outfolder $outfolder --n 1
   else #[ $mode != "pacbio" ] && [ $mode != "'ont'" ]
        /usr/bin/time -v  isONclust  --t $num_cores   --fastq $raw_reads \
-             --outfolder $outfolder/clustering
+             --outfolder $outfolder
        /usr/bin/time -v isONclust write_fastq --N $iso_abundance --clusters $outfolder/clustering/final_clusters.tsv \
                       --fastq $raw_reads --outfolder  $outfolder/clustering/fastq_files
 
@@ -171,18 +183,18 @@ if [ -n "$isONform_folder"  ] #the user has given a path to isONform (cloned fro
   then
   if [ $mode != "pacbio" ] #i.e. we run in ONT mode
     then
-        /usr/bin/time -v  $isONform_folder/isONform_parallel --t $num_cores  --fastq_folder $outfolder/correction/ --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches  --delta_iso_len_3 30 --delta_iso_len_5 50
+        /usr/bin/time -v  $isONform_folder/isONform_parallel --t $num_cores  --fastq_folder $outfolder/correction --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --delta_iso_len_3 30 --delta_iso_len_5 50
   else #we run isONform in pacbio mode (adding the keyword clustered to the command)
-        /usr/bin/time -v   $isONform_folder/isONform_parallel --t $num_cores --fastq_folder $outfolder/clustering/fastq_files --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches --delta_iso_len_3 30 --delta_iso_len_5 50 --clustered
+        /usr/bin/time -v   $isONform_folder/isONform_parallel --t $num_cores --fastq_folder $outfolder/clustering/fastq_files --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance  --delta_iso_len_3 30 --delta_iso_len_5 50
   fi
 
 
-else #the user has not given a path to isONform (pip installation
+else #the user has not given a path to isONform (pip installation supposed)
   if [ $mode != "pacbio" ] #i.e. we run in ONT mode
     then
-        /usr/bin/time -v  isONform_parallel --t $num_cores  --fastq_folder $outfolder/correction/ --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches  --delta_iso_len_3 30 --delta_iso_len_5 50
+        /usr/bin/time -v  isONform_parallel --t $num_cores  --fastq_folder $outfolder/correction/ --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance  --delta_iso_len_3 30 --delta_iso_len_5 50
   else #we run isONform in pacbio mode (adding the keyword clustered to the command)
-        /usr/bin/time -v   isONform_parallel --t $num_cores --fastq_folder $outfolder/clustering/fastq_files --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches --delta_iso_len_3 30 --delta_iso_len_5 50 --clustered
+        /usr/bin/time -v   isONform_parallel --t $num_cores --fastq_folder $outfolder/clustering/fastq_files --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --delta_iso_len_3 30 --delta_iso_len_5 50 
   fi
 fi
 echo
